8bb8c1a2bee343d4550f767ff7034d97688e5b50
goodluck4848 <44718024+goodluck4848@users.noreply.github.com>
2018-11-27 19:47:28 -0500

    Add files via upload

---
+++ truffle-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  networks: {
+    development: {
+       host: "127.0.0.1",
+      port: 8545,
+      gas: 6500000,
+      network_id: "*"
+    }
+  },
+  solc: {
+     optimizer: {
+       enabled: true,
+       runs: 200
+     }
+  }
+};
